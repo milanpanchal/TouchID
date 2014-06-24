5d581e8ebac44ea0bfaad324547b7412d6947259
JamesHarnett <harnett.james@gmail.com>
2014-06-24 17:37:20 -0400

    Objective-C Project Completed
    The Objective-C Xcode project is completed - the Swift code has not yet
    been made available for the LAContext API.

--- objective-c/TouchID/TouchID/ViewController.m
+++ objective-c/TouchID/TouchID/ViewController.m
@@ -24,7 +24,7 @@
 {
     LAContext *myContext = [[LAContext alloc] init];
     NSError *authError = nil;
-    NSString *myLocalizedReasonString = @"Please scan your fingerprint to continue.";
+    NSString *myLocalizedReasonString = @"Please authenticate with your fingerprint to continue.";
     
     if ([myContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&authError]) {
         [myContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
@@ -32,24 +32,12 @@
                             reply:^(BOOL success, NSError *error) {
                                 if (success) {
                                     // User authenticated successfully, take appropriate action
-                                    NSLog(@"authentication success");
-                                    if (!success) {
-                                        NSLog(@"%@", error);
-                                    }
                                 } else {
                                     // User did not authenticate successfully, look at error and take appropriate action
-                                    NSLog(@"authentication failed");
-                                    if (!success) {
-                                        NSLog(@"%@", error);
-                                    }
                                 }
                             }];
     } else {
         // Could not evaluate policy; look at authError and present an appropriate message to user
-        NSLog(@"an error occured");
-        if (!success) {
-            NSLog(@"%@", error);
-        }
     }
 }
 
